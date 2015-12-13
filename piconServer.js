a43a0160eb2baf02025b047b6dc1e56f2574e7b0
Ronaldo Barbachano <redcap3000@gmail.com>
2015-12-13 23:24:41 +0000

    adding missing init function

--- piconServer.js
+++ piconServer.js
@@ -1,5 +1,16 @@
+Meteor.startup(function(){
+
+	Meteor.call('piconInit');
+
+});
+
 Meteor.methods({
-    'set_pixel' : function(x,y,r,g,b){
+	'piconInit' : function(){
+		// run on startup ...
+		RunCli.run('chmod -R 755 assets/app/');
+		Meteor.call('clear_pixel');
+	},
+       'set_pixel' : function(x,y,r,g,b){
 	        RunCli.run('./assets/app/set_pixel.py ' + x + ' ' + y + ' '+ r + ' ' + g + ' ' + b); 
 	},
 	'clear_pixel' : function(r,g,b){
