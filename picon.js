8b169a8d4b19606fa1ff48de8d11661890af6e17
System Administrator <root@Ronaldos-iMac.local>
2015-12-14 14:26:12 -0900

    code cleanup, refactoring rendered flow

--- picon.js
+++ picon.js
@@ -1,33 +1,26 @@
 // Write your package code here!
 Template.picon.rendered = function(){
 	// flush all pixels ?
-	Session.setDefault("picolor",'0 0 0');
-	Meteor.call("clear_pixel");
+	Meteor.call("clear_pixel",function(){
+		Session.set("picolor",'0 0 0');
+	});
 };
 
 Template.picon.helpers ={
 	getCurrentColor : function(){
-		console.log('gettin cur color');
 		var curColors = Session.get('picolor').split(' ');
-		var theCell = document.getElementById( "color" );
+		var theCell = document.getElementById( "currentColor" );
 		var newColor = 'rgb('+curColors.join(',')+')';
 	}
 };
 Template.picon.events = {
 	'click .picon td' : function(evt,tmpl){
-		console.log(this);
-		console.log(evt.currentTarget.id);
 		var cells = evt.currentTarget.id.split(' ');
-		console.log(cells);
-		// get a session color?
-		//console.log(evt.currentTarget.classList);
 		var curColors = Session.get('picolor').split(' ');
 		var theCell = document.getElementById( evt.currentTarget.id );
 		var newColor = 'rgb('+curColors.join(',')+')';
 		theCell.style.backgroundColor = newColor;
-		// whoopps>
 		Meteor.call("set_pixel",cells[1],cells[0],curColors[0],curColors[1],curColors[2]);
-		//theCell.style.backgroundColor='rgb(' + curColors + ');';
 	},
 	'change .piC' : function(evt,tmpl){
 		// find the rgb fields
@@ -36,15 +29,12 @@ Template.picon.events = {
 		var b = tmpl.find("#b").value;
 		// combine and set to session
 		// update 'current color div'
-
-
 		Session.set("picolor", r + ' ' + g + ' ' + b);
-		var theCell = document.getElementById( 'color' );
+		var theCell = document.getElementById( 'currentColor' );
 		var newColor = 'rgb('+r + ',' + g + ',' + b +')';
 		theCell.style.backgroundColor = newColor;
 	},
 	'click .clearPixels' : function(evt,tmpl){
-		// gmm set EVERY cell to the right color?
 		// add option for 'set to new color'
 		console.log('clear pix');
 		Meteor.call("clear_pixel",function(){
